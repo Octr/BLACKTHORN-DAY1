7c3d571fccc95afbb4c3bfca503923e7cbbf8a87
Paris <paris@octr.dev>
2023-07-25 17:56:38 +0100

    Consume Animation

--- Assets/_Game/Scripts/Consume.cs
+++ Assets/_Game/Scripts/Consume.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Consume : MonoBehaviour
 {
+    public Animator animator;
     public OnTriggerEvents onTriggerEvents;
 
     public void Activate()
@@ -11,6 +12,7 @@ public class Consume : MonoBehaviour
         Debug.Log($"Cursor: {onTriggerEvents.gameObject.GetComponent<ColorTag>().color}");
         Debug.Log($"Creature: {onTriggerEvents.triggerData.gameObject.GetComponent<ColorTag>().color}");
         if (onTriggerEvents.gameObject.GetComponent<ColorTag>().color != onTriggerEvents.triggerData.gameObject.GetComponent<ColorTag>().color) return;
+        animator.SetTrigger("Consume");
         onTriggerEvents.triggerData.gameObject.GetComponent<Creature>().Die();
     }
 }
